0ddc5b27f59ca8fd56a233855b5e43d74fd567e7
pravinchoudhary314 <57893509+pravinchoudhary314@users.noreply.github.com>
2022-10-28 16:30:19 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,99 @@
+import 'package:bloc_pattern_demo/counter_bloc.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  final counterBloc = CounterBloc();
+
+  @override
+  Widget build(BuildContext context) {
+    print('----widget tree----');
+
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text(widget.title),
+      ),
+      body: Center(
+
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            StreamBuilder(
+              stream: counterBloc.counterStream,
+              initialData: 0,
+              builder: (context,snapshot){
+                return Text(
+                  '${snapshot.data}',
+                  style: Theme.of(context).textTheme.headline4,
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children:[ FloatingActionButton(
+          onPressed: (){
+            counterBloc.eventSink.add(CounterAction.increment);
+          },
+          tooltip: 'Increment',
+          child: const Icon(Icons.add),
+        ),
+
+          FloatingActionButton(
+            onPressed: (){
+              counterBloc.eventSink.add(CounterAction.decrement);
+            },
+            tooltip: 'Decrement',
+            child: const Icon(Icons.remove),
+          ),
+
+          FloatingActionButton(
+            onPressed: (){
+              counterBloc.eventSink.add(CounterAction.reset);
+            },
+            tooltip: 'Reset',
+            child: const Icon(Icons.loop),
+          ),
+    ]
+      ),
+    );
+  }
+}
